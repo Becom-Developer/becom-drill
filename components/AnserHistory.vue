c576fb2c9d17fa6eff5808984de2877530f2a474
yk <yosizuka1@gmail.com>
2021-11-24 09:17:45 +0900

    anser history

--- components/AnserHistory.vue
+++ components/AnserHistory.vue
@@ -2,63 +2,101 @@
   <div v-if="countHistory" class="my-3 py-3 text-center">
     <h3>かいとうのきろく</h3>
     <div>
-      <b-btn
+      <!-- <b-btn
         v-b-toggle.collapse-1
         variant="outline-success"
         pill
         blocks
         size="lg"
         >せいせきをみる</b-btn
-      >
-      <b-collapse id="collapse-1" class="mt-2">
+      > -->
+      <!-- <b-collapse id="collapse-1" class="mt-2"> -->
         <b-card>
-          <div
-            v-for="card in reportCard"
-            :key="card.id"
-            class="card-text text-center"
-          >
-            <b-btn
-              v-b-toggle="'collapse-' + card.drill.id + '-inner'"
-              size="sm"
-              class="my-1"
-              blocks
-              pill
-              >ドリルID: {{ card.drill.id }}</b-btn
-            >
-            <b-collapse
-              :id="'collapse-' + card.drill.id + '-inner'"
-              class="my-1"
-            >
-              <b-card>
-                <ul>
-                  <li
-                    v-for="record in card.records"
-                    :key="record.id"
-                    class="card-text text-left"
-                  >
-                    第{{ record.recordID }}問: {{ record.question }} =
-                    {{ record.answerResult }}
-                    <span v-if="record.isCorrect" class="">
-                      <b-badge variant="danger">正解</b-badge>
-                    </span>
-                    <span v-else>
-                      <b-badge variant="secondary">不正解</b-badge>
-                    </span>
-                  </li>
-                </ul>
-              </b-card>
-            </b-collapse>
+          <!-- <div>{{reportCardLast}}</div> -->
+          <!-- <div>{{reportCard[0]}}</div> -->
+          <div v-for="last in reportCardLast" :key="last.id">
+            <!-- {{last}} -->
+            <p>さいごのせいせき<br />ドリルID :{{ last.drill.id }}</p>
+            <b-card>
+              <ul>
+                <li
+                  v-for="record in last.records"
+                  :key="record.id"
+                  class="card-text text-left"
+                >
+                  第{{ record.recordID }}問: {{ record.question }} =
+                  {{ record.answerResult }}
+                  <span v-if="record.isCorrect" class="">
+                    <b-badge variant="danger">正解</b-badge>
+                  </span>
+                  <span v-else>
+                    <b-badge variant="secondary">不正解</b-badge>
+                  </span>
+                </li>
+              </ul>
+            </b-card>
           </div>
           <b-btn
-            variant="outline-success"
-            pill
-            blocks
+            v-b-toggle="'morecol'"
             size="sm"
-            @click="removeAnserHistory()"
-            >回答履歴を削除する</b-btn
+            class="my-1"
+            variant="success"
+            blocks
+            pill
+            >もっとみる</b-btn
           >
+          <b-collapse :id="'morecol'" class="my-1"
+            >
+            <div
+              v-for="card in reportCard"
+              :key="card.id"
+              class="card-text text-center"
+            >
+              <b-btn
+                v-b-toggle="'collapse-' + card.drill.id + '-inner'"
+                size="sm"
+                class="my-1"
+                blocks
+                pill
+                >ドリルID: {{ card.drill.id }}</b-btn
+              >
+              <b-collapse
+                :id="'collapse-' + card.drill.id + '-inner'"
+                class="my-1"
+              >
+                <b-card>
+                  <ul>
+                    <li
+                      v-for="record in card.records"
+                      :key="record.id"
+                      class="card-text text-left"
+                    >
+                      第{{ record.recordID }}問: {{ record.question }} =
+                      {{ record.answerResult }}
+                      <span v-if="record.isCorrect" class="">
+                        <b-badge variant="danger">正解</b-badge>
+                      </span>
+                      <span v-else>
+                        <b-badge variant="secondary">不正解</b-badge>
+                      </span>
+                    </li>
+                  </ul>
+                </b-card>
+              </b-collapse>
+            </div>
+          </b-collapse>
+          <div>
+            <b-btn
+              variant="outline-success"
+              pill
+              blocks
+              size="sm"
+              @click="removeAnserHistory()"
+              >回答履歴を削除する</b-btn
+            >
+          </div>
         </b-card>
-      </b-collapse>
+      <!-- </b-collapse> -->
     </div>
   </div>
 </template>
