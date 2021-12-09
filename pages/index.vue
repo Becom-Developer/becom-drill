ba73fe5a987e6d8737c0fb5f30c8f99916f1045e
yk <yosizuka1@gmail.com>
2021-12-09 17:11:12 +0900

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <b-container class="bg-light mt-3 text-center">
     <h1>ドリルマスター</h1>
-    <p>みんなでたのしくグリグリ計算ドリルをやってみよう</p>
+    <p>みんなでたのしく計算ドリルをやってみよう</p>
     <h2>ドリルをえらぼう</h2>
-    <p>4つのドリルがえらべるよ</p>
-    <p>やってみたいドリルをクリックしてみよう</p>
+    <!-- <p>4つのドリルがえらべるよ</p> -->
+    <p>やってみたいドリルをクリック</p>
     <b-row>
       <b-col lg="3" class="pb-2"
         ><b-btn
@@ -16,36 +16,36 @@
           >たしざん</b-btn
         ></b-col
       >
-      <b-col lg="3" class="pb-2"
-        ><b-btn
-          block
-          pill
-          size="lg"
-          variant="outline-secondary"
-          @click="$router.push('/subtraction')"
-          >ひきざん</b-btn
-        ></b-col
-      >
-      <b-col lg="3" class="pb-2"
-        ><b-btn
-          block
-          pill
-          size="lg"
-          variant="outline-secondary"
-          @click="$router.push('/multiplication')"
-          >かけざん</b-btn
-        ></b-col
-      >
-      <b-col lg="3" class="pb-2"
-        ><b-btn
-          block
-          pill
-          size="lg"
-          variant="outline-secondary"
-          @click="$router.push('/division')"
-          >わりざん</b-btn
-        ></b-col
-      >
+      <!-- <b-col lg="3" class="pb-2" -->
+        <!-- ><b-btn -->
+          <!-- block -->
+          <!-- pill -->
+          <!-- size="lg" -->
+          <!-- variant="outline-secondary" -->
+          <!-- @click="$router.push('/subtraction')" -->
+          <!-- >ひきざん</b-btn -->
+        <!-- ></b-col -->
+      <!-- > -->
+      <!-- <b-col lg="3" class="pb-2" -->
+        <!-- ><b-btn -->
+          <!-- block -->
+          <!-- pill -->
+          <!-- size="lg" -->
+          <!-- variant="outline-secondary" -->
+          <!-- @click="$router.push('/multiplication')" -->
+          <!-- >かけざん</b-btn -->
+        <!-- ></b-col -->
+      <!-- > -->
+      <!-- <b-col lg="3" class="pb-2" -->
+        <!-- ><b-btn -->
+          <!-- block -->
+          <!-- pill -->
+          <!-- size="lg" -->
+          <!-- variant="outline-secondary" -->
+          <!-- @click="$router.push('/division')" -->
+          <!-- >わりざん</b-btn -->
+        <!-- ></b-col -->
+      <!-- > -->
     </b-row>
   </b-container>
 </template>
